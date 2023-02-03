8fa1931daf78652e09132d43b7eeaf4906ebcb28
tstrecke <t.strecke@berufskolleg-rheine.de>
2023-02-03 09:53:37 +0100

    Anbindung SQL-Database

--- bestellung.php
+++ bestellung.php
@@ -27,13 +27,41 @@
 $bnr = $_GET["bnr"]; 
 echo "<h1>iBrot-Bestellung</h1>"; 
 echo "<h2>Bestellnummer: $bnr </h2>"; 
+?>
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:str-ibrot-dbsrv.database.windows.net,1433; Database = ibrot", "str", "AzUrE2o23@BKR");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "str", "pwd" => "AzUrE2o23@BKR", "Database" => "ibrot", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:str-ibrot-dbsrv.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
+
+<?php
+/* Set up and execute the query. */  
+$tsql = "SELECT ArtikelID, Position, Menge FROM Bestellposition where BestellungID =" + $bnr;  
+$stmt = sqlsrv_query( $conn, $tsql);  
+if( $stmt === false)  
+{  
+     echo "Error in query preparation/execution.\n";  
+     die( print_r( sqlsrv_errors(), true));  
+}  
+?>
  
 /* Daten in einem assoziativen Array definieren*/ 
-$daten = array ( 
+/*$daten = array ( 
     array("Position"=>1,"ArtikelID"=>"Brötchen","Menge"=>5), 
     array("Position"=>2,"ArtikelID"=>"Croissant","Menge"=>3), 
     array("Position"=>3,"ArtikelID"=>"Brot","Menge"=>1) 
-  ); 
+  );*/ 
  
 /* Überschriften im Tabellen-Head ausgeben */ 
 echo "<table>"; 
@@ -46,17 +74,35 @@ echo "</tr>";
 echo "</thead>";  
  
 /* Datensätze im Tabellen-Body ausgeben */ 
-echo "<tbody>";           
-foreach ($daten as $row) { 
+echo "<tbody>";  
+
+<?php
+/* Retrieve each row as an associative array and display the results.*/  
+while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC))  
+{  
+     echo "<tr>"; 
+     echo "<td>".$row['Position']."</td>"; 
+     echo "<td>".$row['ArtikelID']."</td>"; 
+     echo "<td>".$row['Menge']."</td>"; 
+     echo "</tr>";
+     //echo $row['LastName'].", ".$row['FirstName']."\n";  
+}  
+  
+/* Free statement and connection resources. */  
+sqlsrv_free_stmt( $stmt);  
+sqlsrv_close( $conn);  
+?>  
+
+/*foreach ($daten as $row) { 
     echo "<tr>"; 
     echo "<td>".$row['Position']."</td>"; 
     echo "<td>".$row['ArtikelID']."</td>"; 
     echo "<td>".$row['Menge']."</td>"; 
     echo "</tr>";   
-} 
+} */
 echo "</table>"; 
 echo "<tbody>";  
    
-?>                       <!-- Ende PHP--> 
+//?>                       <!-- Ende PHP--> 
 </body>                  <!-- Ende HTML-Body--> 
 </html>                  <!-- Ende HTML--> 
